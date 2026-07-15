397695144be0ea94e0aa603f043d5722a73be013
Your Name <you@example.com>
2026-07-15 16:34:21 +0530

    ain

--- main.py
+++ main.py
@@ -1,39 +1,91 @@
+# =====================================
+# Python Calculator
+# Created by: Sushanth
+# Version: 1.1
+# =====================================
+def add(num1: float, num2: float) -> float:
+    return num1 + num2
 
-print("================================")
-print("        PYTHON CALCULATOR")
-print("================================")
-print(" 1. Addition")
-print(" 2. Substraction")
-print(" 3. Multiplication")
-print(" 4. Division")
-print(" 5. Exit")
+def subtract(num1: float, num2: float) -> float: 
+    return num1 - num2 
 
-choice = input("Enter your choice (1/2/3/4/5): ")
+def multiply(num1: float, num2: float) -> float: 
+    return num1 * num2 
 
-if choice == "1":
-    num1 = float(input("enter your first number:"))
-    num2 = float(input("enter your second number:"))
-    result = num1 + num2
-    print("The result of the addition is:", result)
-elif choice == "2":
-    num1 = float(input("eneter your first number:"))
-    num2 = float(input("enter your second number:"))
-    result = num1 - num2
-    print("the result of the subtraction is:",result)
-elif choice == "3":
-    num1 = float(input("enter your first number:"))
-    num2 = float(input("enter your second number:"))
-    result = num1 * num2
-    print("Result of the mutiplication:",result)
-elif choice == "4":
-    num1 = float(input("enter your first number:"))
-    num2 = float(input("enter your second number:"))
+def divide(num1: float, num2: float) -> float | str:  
     if num2 == 0:
-        print("error: Division by Zero is not allowed")
+        return "error: division by zero"
     else:
-        result = num1/num2
-        print("Result of Division:",result)
-elif choice == "5":
-    print("Exiting calculator...")
-else:
-    print("invalid choice. please select a valid option (1/2/3/4/5).")
+        return num1 / num2 
+    
+history: list[str] = []
+    
+
+while True:
+    print("\n================================")
+    print("        PYTHON CALCULATOR")
+    print("================================")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1/2/3/4/5): ")
+
+    if choice == "1":
+        try:
+            num1 = float(input("Enter your first number: "))
+            num2 = float(input("Enter your second number: "))
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+        else:
+            result = add(num1,num2)
+            print(f"Result: {result}")
+
+            history.append(f"{num1} + {num2} = {result}") 
+
+    elif choice == "2":
+        try:
+            num1 = float(input("Enter your first number: "))
+            num2 = float(input("Enter your second number: "))
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+        else:
+            result = subtract(num1,num2)
+            print(f"Result: {result}")
+
+            history.append(f"{num1} - {num2} = {result}")
+
+    elif choice == "3":
+        try:
+            num1 = float(input("Enter your first number: "))
+            num2 = float(input("Enter your second number: "))
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+        else:
+            result = multiply(num1,num2)
+            print(f"Result: {result}")
+
+            history.append(f"{num1} * {num2} = {result}")
+
+    elif choice == "4":
+        try:
+            num1 = float(input("Enter your first number: "))
+            num2 = float(input("Enter your second number: "))
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+        else:
+            result = divide(num1,num2)
+            print(f"Result: {result}")
+
+            history.append(f"{num1} / {num2} = {result}")
+
+    elif choice == "5":
+        print("Exiting calculator...")
+        break
+
+    else:
+        print("Invalid choice. Please enter 1-5.")
+
+    
